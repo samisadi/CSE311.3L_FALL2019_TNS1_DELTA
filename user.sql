93ceda579ccc63d85a66b0b10ce03ef8e3e95204
samisadi <57316636+samisadi@users.noreply.github.com>
2019-12-14 23:23:43 +0600

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `foodie`.`user` ( `id` INT(5) NOT NULL AUTO_INCREMENT , `first_name` TEXT NOT NULL , `middle_name` TEXT NOT NULL , `last_name` TEXT NOT NULL , `user_name` VARCHAR(50) NOT NULL , `password` VARCHAR(50) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
